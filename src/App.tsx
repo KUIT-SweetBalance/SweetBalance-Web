4890df70f25f7e4a6ab8905f698c50346fa219b6
Hwanggyuun <gka365@naver.com>
2025-01-09 16:38:05 +0900

    add custom main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+import CustomMain from './pages/custom/custom-main/custom';
 // import TailwindExample from './pages/TailwindExample';
 // import UserNickname from './pages/onboarding/UserNickname';
 import UserPassword from './pages/onboarding/UserPassword';
-
-function App() {
+interface Props {
+  brand : string;
+  drink : string;
+  sugar : number;
+  kcal: number;
+  caffeine: number;
+}
+const  App=()=> {
   return (
-    <UserPassword />
+    <CustomMain brand="스타벅스" drink="아이스 아메리카노" sugar={0} kcal={0} caffeine={5}/>
   );
 }
 

33df092735261e623b63179e48b424b0cf5c6c9b
Hwanggyuun <gka365@naver.com>
2025-01-15 15:23:00 +0900

    마이 페이지 수정하기 구현완료료

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Mypages from './mypage-main/Mypages';
+import Mypagerevise from './mypage-revise/Mypagerevise';
 
 const Mypage: React.FC = () => {
     return (
         <>
         {/* <Mypages/>메인 페이지  */}
+        <Mypagerevise/>
         </>
     );
 };

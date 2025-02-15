fa7fbb23c8d573ffdf677f4bc2bda0e4ea683f56
jstar_00 <wltjd6300@naver.com>
2025-02-15 19:11:52 +0900

    [Setting] netlify setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],

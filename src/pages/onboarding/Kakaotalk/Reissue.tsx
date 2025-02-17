deb8bbc86bc4b19a48ecf6c2bdd94cb2071de83a
Hwanggyuun <gka365@naver.com>
2025-02-17 23:37:11 +0900

    logout4

--- src/pages/onboarding/Kakaotalk/Reissue.tsx
+++ src/pages/onboarding/Kakaotalk/Reissue.tsx
@@ -45,6 +45,7 @@ const Reissue: React.FC = () => {
       // ✅ ApiManager의 Authorization 헤더 업데이트
       ApiManager.defaults.headers.Authorization = `Bearer ${response.data.data.access}`;
       document.cookie = "refreshToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC; secure; SameSite=None";
+      document.cookie = "refresh=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC; secure; SameSite=None";
       document.cookie = `refresh=${response.data.data.refresh}; path=/; secure; SameSite=None`;
 
       console.log("🚮 Refresh Token 쿠키 삭제 완료");

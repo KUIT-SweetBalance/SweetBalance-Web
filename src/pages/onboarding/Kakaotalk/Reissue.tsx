cffd932884ae690bb64d8bbe56f812a54c128d0d
Hwanggyuun <gka365@naver.com>
2025-02-17 14:36:58 +0900

    add body

--- src/pages/onboarding/Kakaotalk/Reissue.tsx
+++ src/pages/onboarding/Kakaotalk/Reissue.tsx
@@ -11,27 +11,28 @@ const Reissue: React.FC = () =>{
         const refreshToken = urlParams.get("refresh");
         const saveRefreshTokenToCookie = (refreshToken: string) => {
             document.cookie = `refreshToken=${refreshToken}; path=/; secure; HttpOnly;`;
+            console.log("쿠키 저장완료")
           };
         if (refreshToken) {
           console.log("🔄 Refresh Token 확인:", refreshToken);
           saveRefreshTokenToCookie(refreshToken);
-          reissueToken();
+          reissueToken(refreshToken);
         }
       }, []);
       
-      const reissueToken = async () => {
+      const reissueToken = async (refreshToken: string) => {
         try {
           const response = await axios.post("/api/auth/reissue", 
-            // { refreshToken },
+            { refreshToken },
             { withCredentials: true } // 쿠키도 자동으로 포함
           );
     
           console.log("✅ Access Token 재발급 성공:", response.data.accessToken);
           localStorage.setItem("accessToken", response.data.accessToken);
-          navigate("/home"); 
+        //   navigate("/home"); 
         } catch (error) {
           console.error("❌ Access Token 재발급 실패:", error);
-          navigate("/auth-selection"); 
+        //   navigate("/auth-selection"); 
 
         }
       };
